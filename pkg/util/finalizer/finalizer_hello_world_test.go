addcb41e894d82e07e402612f652d6338e8305db
Taras Prokhorov <Taras_Prokhorov@epam.com>
2020-08-12 13:49:05 +0300

    test with GOlang file#15
    Change-Id: Iaf37aee4b62bc667c159cd73f983ef4aa788f65d

---
+++ pkg/util/finalizer/finalizer_hello_world_test.go
@@ -0,0 +1,71 @@
+package finalizer
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestContainsStringHelloWorld(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld66(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld6(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld5(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld4(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld3(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld2(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld111(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld15(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld14(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld13(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld12(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld11(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
