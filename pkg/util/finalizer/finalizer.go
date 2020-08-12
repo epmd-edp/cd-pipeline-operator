b9c077369ab095bc1b96bce4536b2138fef1e43c
Taras Prokhorov <Taras_Prokhorov@epam.com>
2020-08-12 13:49:05 +0300

    test with GOlang file#20
    Change-Id: Iaf37aee4b62bc667c159cd73f983ef4aa788f65d

--- pkg/util/finalizer/finalizer.go
+++ pkg/util/finalizer/finalizer.go
@@ -8,3 +8,30 @@ func ContainsString(slice []string, s string) bool {
 	}
 	return false
 }
+
+func ContainsStringIntest(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsStringIntesting(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsStringIntestingfile(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
