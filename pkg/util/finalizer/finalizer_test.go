7ef1ee760dac06bc940d9373709d38a318e12a73
Taras Prokhorov <Taras_Prokhorov@epam.com>
2020-08-12 13:49:05 +0300

    test with GOlang file#1
    Change-Id: Iaf37aee4b62bc667c159cd73f983ef4aa788f65d

---
+++ pkg/util/finalizer/finalizer_test.go
@@ -0,0 +1,46 @@
+package finalizer
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestContainsString(t *testing.T) {
+	e := ContainsString([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsString66(t *testing.T) {
+	e := ContainsString([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsString6(t *testing.T) {
+	e := ContainsString([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsString5(t *testing.T) {
+	e := ContainsString([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsString4(t *testing.T) {
+	e := ContainsString([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsString3(t *testing.T) {
+	e := ContainsString([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsString2(t *testing.T) {
+	e := ContainsString([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsString1(t *testing.T) {
+	e := ContainsString([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
