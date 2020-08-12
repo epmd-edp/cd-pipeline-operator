dba0506c9a30578e7d95ac7dc4eba7a517a87ec1
Taras Prokhorov <Taras_Prokhorov@epam.com>
2020-08-12 13:49:05 +0300

    test with GOlang file#48master
    Change-Id: Iaf37aee4b62bc667c159cd73f983ef4aa788f65d

---
+++ pkg/util/finalizer/finalizer_hello_Devops.go
@@ -0,0 +1,28 @@
+package finalizer
+
+func ContainsStringHelloDevops(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsStringHelloDevopsTest(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsStringHelloDevopsTesting(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
