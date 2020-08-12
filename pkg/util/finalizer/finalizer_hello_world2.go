b6f1379f501720931e0004489a0e7a701177525e
Taras Prokhorov <Taras_Prokhorov@epam.com>
2020-08-12 13:49:05 +0300

    test with GOlang file#13
    Change-Id: Iaf37aee4b62bc667c159cd73f983ef4aa788f65d

---
+++ pkg/util/finalizer/finalizer_hello_world2.go
@@ -0,0 +1,10 @@
+package finalizer
+
+func ContainsStringHelloWorld2(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
---
+++ pkg/util/finalizer/finalizer_hello_world_test2.go
@@ -0,0 +1,71 @@
+package finalizer
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestContainsStringHelloWorld2(t *testing.T) {
+	e := ContainsStringHelloWorld22([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld266(t *testing.T) {
+	e := ContainsStringHelloWorld22([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld26(t *testing.T) {
+	e := ContainsStringHelloWorld22([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld25(t *testing.T) {
+	e := ContainsStringHelloWorld22([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld24(t *testing.T) {
+	e := ContainsStringHelloWorld22([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld23(t *testing.T) {
+	e := ContainsStringHelloWorld22([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld22(t *testing.T) {
+	e := ContainsStringHelloWorld2([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld2111(t *testing.T) {
+	e := ContainsStringHelloWorld2([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld215(t *testing.T) {
+	e := ContainsStringHelloWorld2([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld214(t *testing.T) {
+	e := ContainsStringHelloWorld2([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld213(t *testing.T) {
+	e := ContainsStringHelloWorld2([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld212(t *testing.T) {
+	e := ContainsStringHelloWorld2([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
+
+func TestContainsStringHelloWorld211(t *testing.T) {
+	e := ContainsStringHelloWorld2([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
