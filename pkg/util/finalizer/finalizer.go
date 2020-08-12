702654aa7fbf687c56f82058d1c04e904d702f09
Taras Prokhorov <Taras_Prokhorov@epam.com>
2020-08-12 13:49:05 +0300

    test with GOlang file#37
    Change-Id: Iaf37aee4b62bc667c159cd73f983ef4aa788f65d

--- pkg/util/finalizer/finalizer.go
+++ pkg/util/finalizer/finalizer.go
@@ -8,3 +8,12 @@ func ContainsString(slice []string, s string) bool {
 	}
 	return false
 }
+
+func ContainsStringOne(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
---
+++ pkg/util/finalizer/finalizer_test.go
@@ -0,0 +1,14 @@
+package finalizer
+import (
+   "github.com/stretchr/testify/assert"
+   "testing"
+)
+func TestContainsString(t *testing.T) {
+   e := ContainsString([]string{"a", "b"}, "b")
+   assert.True(t, e)
+}
+
+func TestContainsStringOne(t *testing.T) {
+   e := ContainsString([]string{"a", "b"}, "b")
+   assert.True(t, e)
+}
