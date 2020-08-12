c44dd7738e26e7583327419b1cb682b5af038969
Taras Prokhorov <Taras_Prokhorov@epam.com>
2020-08-12 13:49:05 +0300

    test with GOlang file#41
    Change-Id: Iaf37aee4b62bc667c159cd73f983ef4aa788f65d

---
+++ pkg/util/finalizer/finalizer_hello_world_test.go
@@ -0,0 +1,11 @@
+package finalizer
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestContainsStringHelloWorld(t *testing.T) {
+	e := ContainsStringHelloWorld([]string{"a", "b"}, "b")
+	assert.True(t, e)
+}
